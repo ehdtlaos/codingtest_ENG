835f592661659b5352f1cf633920e6ce82e65867
ehdtlaos <eee910413@gmail.com>
2021-10-17 15:03:49 -0700

    dropdown fix

--- src/Pages/styles/BeerList.js
+++ src/Pages/styles/BeerList.js
@@ -4,16 +4,16 @@ export const Center = styled.nav`
   display: flex;
   flex-direction: column;
   padding: 0.5rem calc((100vw - 1000px) / 2);
-  z-index: -100;
 `
 
 export const CenterOne = styled.nav`
   display: flex;
   margin-bottom: 10px;
-
+  z-index: 10;
 `
 
 export const CenterTwo = styled.nav`
   display: flex;
+  z-index: 1;
 
 `
